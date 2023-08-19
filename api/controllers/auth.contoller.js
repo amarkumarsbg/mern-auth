506295cc67ce85be7c3a22c576b625a991b12c9c
amarkumarsbg <amarkumarsbg13@gmail.com>
2023-08-19 21:47:21 +0530

    complete the sign in page functionality

--- api/controllers/auth.contoller.js
+++ api/controllers/auth.contoller.js
@@ -7,7 +7,6 @@ export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
-
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
@@ -18,17 +17,16 @@ export const signup = async (req, res, next) => {
 
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
-
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(404, " User not found"));
+    if (!validUser) return next(errorHandler(404, "User not found"));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, " wrong credentials"));
+    if (!validPassword) return next(errorHandler(401, "wrong credentials"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: hashedPassword, ...rest } = validUser._doc;
-    const expiryDate = new Date(Date.now() + 3600000); //1 hour
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
     res
-      .cookie("access_token", token, { httpOnly: true }, (maage = 3600000))
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
       .status(200)
       .json(rest);
   } catch (error) {
