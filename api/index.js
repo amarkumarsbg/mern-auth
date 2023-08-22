c38257651d471a9b64f24711adecac4f92e0d261
amarkumarsbg <amarkumarsbg13@gmail.com>
2023-08-22 21:52:09 +0530

    create update user api route

--- api/index.js
+++ api/index.js
@@ -3,12 +3,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("connected to MongoDB");
+    console.log("Connected to MongoDB");
   })
   .catch((err) => {
     console.log(err);
@@ -17,6 +18,8 @@ mongoose
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
